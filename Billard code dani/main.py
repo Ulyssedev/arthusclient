cc9d27fe04f8b4c3fc0aab1d9b7a563bbd97f67c
Dan1P0t1 <102626859+Dan1P0t1@users.noreply.github.com>
2022-04-14 21:48:46 +0200

    J'ai tenté de définir une rotation

--- Billard code dani/main.py
+++ Billard code dani/main.py
@@ -3,15 +3,14 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-#ecran
-pygame.display.set_caption("Billard")
+
+pygame.display.set_caption("Billard Du Daniel")
 screen = pygame.display.set_mode((1920,1080))
 
-#icone
+
 icon_32x32 = pygame.image.load("icones/billard.png")
 pygame.display.set_icon(icon_32x32)
 
-#background
 bg = pygame.image.load("cartes/salle2.png")
 
 FPS = 165
@@ -21,19 +20,20 @@ corona = pygame.mixer.Sound("Sons/corona.wav")
 belayarika = pygame.mixer.Sound("Sons/belayarika.wav")
 
 game = Game()
-
+imcount = 0
 running = True
 
 quille_x = 0
 quille_y = 0
 
-
-#belayarika.play()
+rotated_image = game.player.image
 
 lock = 0
 angle = 0
-#boucle pour pas fermer fenetre
+
+
 while running:
+    game.player.image = rotated_image
     screen.blit(bg, (0, 0))
     screen.blit(game.player.image, (quille_x, quille_y))
     pygame.display.flip()
@@ -46,10 +46,16 @@ while running:
                 quille_x = event.pos[0]
                 quille_y = event.pos[1]
                 screen.blit(game.player.image, (quille_x, quille_y))
-                #heheha.play()
+                heheha.play()
             elif event.button == 2:
                 if lock == 0:
                     lock = 1
                 elif lock == 1:
                     lock = 0
+            elif lock == 1 and event.button == 3:
+                rotated_image = pygame.transform.rotate(game.player.image, angle)
+                angle += 1
+                screen.blit(rotated_image, (quille_x, quille_y))
+                #game.player.image = pygame.transform.rotate(game.player.image, 1)
+                
     
